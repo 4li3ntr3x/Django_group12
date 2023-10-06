da1774be2c2792d92aadd46c485324d808cf307e
Horacio Peña <proyectoshep@gmail.com>
2023-10-06 15:40:49 -0300

    Conexion a BBDD

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,11 @@
 from django.db import models
 
+
 class Post(models.Model):
-    titulo = models.CharField(max_length=200)
-    contenido = models.TextField()
-    autor = models.CharField(max_length=100)
-    fecha = models.DateField(auto_now_add=True)
+    titulo = models.CharField(max_length=200, null=True)
+    contenido = models.TextField(max_length=200, null=True)
+    autor = models.CharField(verbose_name="Autor", max_length=100, null=True)
+    fecha = models.DateField(null=True)
 
-    def __str__(self):
-        return self.titulo
-
-
-
+    """def __str__(self):
+        return self.titulo"""
